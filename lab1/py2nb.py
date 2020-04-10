f9da243ff15b8aa2355066e6cf078d56df1a169a
ruben <rubengargiulo@yahoo.it>
2020-04-10 16:25:22 +0200

    10/04/2020_16:25:22

--- lab1/py2nb.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python3
-import os
-for py in os.listdir("py"):
-    if py.endswith(".py"):
-        nb = py.replace(".py", ".ipynb")
-        print(f"{py} to {nb}")
-        os.system(f"ipynb-py-convert py/{py} nb/{nb}")
