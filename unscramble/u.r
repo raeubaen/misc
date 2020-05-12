6389166e5bd1b1f1cf40bf6f5b30b8d33b2c454b
ruben <rubengargiulo@yahoo.it>
2020-05-12 16:03:27 +0200

    12/05/2020_16:03:27

--- unscramble/u.r
+++ unscramble/u.r
@@ -3,7 +3,7 @@ library(imager)
 library(purrr)
 
 im <- load.image("https://camo.githubusercontent.com/7d413e9a343e8ceaa507d68a1a6e93247d5f7853/68747470733a2f2f726f62696e686f7573746f6e2e6769746875622e696f2f696d6167652d756e736872656464696e672f696d616765732f6f726967696e616c2f626c75652d686f75722d70617269732e706e67")
-
+#im <- load.image("~/2.png")
 scramble <- function(im,axis="x")
 {
     imsplit(im,axis) %>% { .[sample(length(.))] } %>% imappend(axis) 
